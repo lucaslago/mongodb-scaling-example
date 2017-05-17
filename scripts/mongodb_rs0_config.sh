7051d792c04e8cf95251253ed4e92136c9e805fe
Lucas Lago <lucasbenderlago@gmail.com>
2017-05-17 17:26:04 -0300

    Rename first replicaset

--- scripts/mongodb_rs0_config.sh
+++ scripts/mongodb_rs0_config.sh
@@ -1,21 +1,21 @@
-mongo --host mongodb1:27017 <<EOF
+mongo --host mongodb1 <<EOF
    var cfg = {
-        "_id": "rs",
+        "_id": "rs0",
         "version": 1,
         "members": [
             {
                 "_id": 0,
-                "host": "mongodb1:27017",
+                "host": "mongodb1",
                 "priority": 2
             },
             {
                 "_id": 1,
-                "host": "mongodb2:27017",
+                "host": "mongodb2",
                 "priority": 0
             },
             {
                 "_id": 2,
-                "host": "mongodb3:27017",
+                "host": "mongodb3",
                 "priority": 0
             },
         ]
--- scripts/shard_config.sh
+++ scripts/shard_config.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 mongo --host mongodbRouter <<EOF
-  db.adminCommand( { addshard : "rs/mongodb1" } );
+  db.adminCommand( { addshard : "rs1/mongodb1" } );
   db.adminCommand({enableSharding: "test"})
   db.adminCommand({shardCollection: "test.kittens", key: {kittens_id:1}});
 EOF
